fa6169eba58ccb8c5cc1fe946a1215f28d9a0efa
humberto.dutra@hotmail.com <humberto.dutra@hotmail.com>
2022-09-06 16:05:56 -0300

    upsidedown

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const strangerThingsService = new StrangerThingsService(
 
 app.use(cors());
 
-const hereIsTheUpsideDown = true;
+const hereIsTheUpsideDown = process.env.UPSIDEDOWN_MODE;
 
 app.get('/', (req, res) => {
   const characters = strangerThingsService.search(
